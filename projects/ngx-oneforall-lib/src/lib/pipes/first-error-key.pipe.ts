48529b771b68793880b8651d116b6593f7e2c039
LPSingh <lsk18mto@gmail.com>
2025-03-31 21:53:44 -0400

    Added first error key pipe

---
+++ projects/ngx-oneforall-lib/src/lib/pipes/first-error-key.pipe.ts
@@ -0,0 +1,48 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+/**
+ * A custom Angular pipe that retrieves the first error key from an `AbstractControl` instance.
+ * This is useful for displaying the first validation error in a form control.
+ *
+ * @example
+ * <!-- Use directly with a form control -->
+ *  <form>
+ *     <input [formControl]="formControl" placeholder="Enter value" />
+ *     @if (formControl | firstErrorKey as firstError) {
+ *       <div>
+ *          First error key: {{ firstError }}
+ *        </div>
+ *     }
+ *  </form>
+ * 
+ * <!-- Use with validation errors -->
+ *  <form>
+ *     <input [formControl]="formControl" placeholder="Enter value" />
+ *     <mat-error>
+ *        {{ formControl.errors | firstErrorKey }}
+ *     </mat-error>
+ *  </form>
+ *
+ * @export
+ * @class firstErrorKey
+ * @implements {PipeTransform}
+ */
+@Pipe({
+    name: 'firstErrorKey'
+})
+export class FirstErrorKeyPipe implements PipeTransform {
+    transform(input?: ValidationErrors | AbstractControl | null): any {
+        if(!input) {
+            return null;
+        }
+
+        const errors: ValidationErrors | null = input instanceof AbstractControl ? input.errors : input;
+        if(!errors) {
+            return null;
+        }
+
+        const errorKeys = Object.keys(errors);
+        return errorKeys.length > 0 ? errorKeys[0] : null;
+    }
+}
