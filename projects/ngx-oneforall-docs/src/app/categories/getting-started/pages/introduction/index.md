00a42692a5e43d4e3a8c288f2ec44b4af1ff1bfb
LPSingh <lsk18mto@gmail.com>
2025-04-21 12:34:21 -0400

    Added introduction cateogry

---
+++ projects/ngx-oneforall-docs/src/app/categories/getting-started/pages/introduction/index.md
@@ -0,0 +1,3 @@
+
+
+Introduction to Ngx One for all
---
+++ projects/ngx-oneforall-docs/src/app/categories/getting-started/pages/introduction/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import GettingStartedCategory from '../../ng-doc.category';
+
+const Introduction: NgDocPage = {
+  title: 'Installation',
+  mdFile: './index.md',
+  order: 0,
+  category: GettingStartedCategory,
+};
+
+export default Introduction;
