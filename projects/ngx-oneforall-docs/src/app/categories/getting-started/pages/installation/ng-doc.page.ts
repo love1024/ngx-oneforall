01f24485fce52d7cb33aa7630084b0071f7f6087
LPSingh <lsk18mto@gmail.com>
2025-04-23 21:49:39 -0400

    Updated order for getting started

--- projects/ngx-oneforall-docs/src/app/categories/getting-started/pages/installation/ng-doc.page.ts
+++ projects/ngx-oneforall-docs/src/app/categories/getting-started/pages/installation/ng-doc.page.ts
@@ -4,6 +4,7 @@ import GettingStartedCategory from '../../ng-doc.category';
 const Installation: NgDocPage = {
   title: 'Installation',
   mdFile: './index.md',
+  order: 1,
   category: GettingStartedCategory,
 };
 
