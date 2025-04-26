ab62238155600313032a92c53b17a85fbb50440e
LPSingh <lsk18mto@gmail.com>
2025-04-25 23:23:33 -0400

    Added docs for hash

---
+++ projects/ngx-oneforall-docs/src/app/categories/utils/pages/hash/ng-doc.page.ts
@@ -0,0 +1,10 @@
+import { NgDocPage } from '@ng-doc/core';
+import UtilsCategory from '../../ng-doc.category';
+
+const HashUtil: NgDocPage = {
+  title: 'Hash',
+  mdFile: './index.md',
+  category: UtilsCategory,
+};
+
+export default HashUtil;
