8f5f18b1a00305d9d0d18cb335edd8347d7ac152
LPSingh <lsk18mto@gmail.com>
2025-05-13 22:58:39 -0400

    chore: added network-status doc structure

---
+++ projects/ngx-oneforall-docs/src/app/categories/services/pages/network-status/ng-doc.page.ts
@@ -0,0 +1,10 @@
+import { NgDocPage } from '@ng-doc/core';
+import ServicesCategory from '../../ng-doc.category';
+
+const NetworkStatusService: NgDocPage = {
+  title: 'Network Status',
+  mdFile: './index.md',
+  category: ServicesCategory,
+};
+
+export default NetworkStatusService;
