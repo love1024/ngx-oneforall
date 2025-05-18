0f1094e53ba9c0ade362164dc8c12e24f8faf134
LPSingh <lsk18mto@gmail.com>
2025-05-17 22:52:01 -0400

    feat: added unsaved changes can deactive guard

---
+++ projects/ngx-oneforall-lib/guards/src/public_api.ts
@@ -0,0 +1,1 @@
+export * from './unsaved-changes/unsaved-changes.guard';
---
+++ projects/ngx-oneforall-lib/guards/src/unsaved-changes/unsaved-changes.guard.ts
@@ -0,0 +1,36 @@
+import { DOCUMENT } from '@angular/common';
+import { inject } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+
+export interface HasUnsavedChanges {
+  hasUnsavedChanges: () => boolean | Observable<boolean> | Promise<boolean>;
+}
+
+export const unsavedChangesGuard = (
+  message = 'You have unsaved changes. Do you want to leave this page?'
+) => {
+  const window = inject(DOCUMENT).defaultView;
+
+  return (component: HasUnsavedChanges) => {
+    if (!window) {
+      return true;
+    }
+    const result = component.hasUnsavedChanges();
+    if (typeof result === 'boolean') {
+      return result ? window.confirm(message) : true;
+    }
+
+    let hasChangesPromise = Promise.resolve(false);
+    if (result instanceof Observable) {
+      hasChangesPromise = lastValueFrom(result);
+    } else if (result instanceof Promise) {
+      hasChangesPromise = result;
+    }
+    return hasChangesPromise.then(hasChanges => {
+      if (hasChanges) {
+        return window.confirm(message);
+      }
+      return true;
+    });
+  };
+};
