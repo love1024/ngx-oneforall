ec20729313aede640cf725d2205ec01f92930352
LPSingh <lsk18mto@gmail.com>
2025-06-03 22:29:41 -0400

    feat: added click outside directive

---
+++ projects/ngx-oneforall-lib/directives/src/click-outside/click-outside.directive.ts
@@ -0,0 +1,53 @@
+import { DOCUMENT } from '@angular/common';
+import {
+  afterNextRender,
+  booleanAttribute,
+  Directive,
+  ElementRef,
+  inject,
+  input,
+  NgZone,
+  OnDestroy,
+  output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[clickOutside]',
+})
+export class ClickOutsideDirective implements OnDestroy {
+  clickOutside = output<Event>();
+  clickOutsideEnabled = input(true, { transform: booleanAttribute });
+
+  private readonly document = inject(DOCUMENT);
+  private readonly ngZone = inject(NgZone);
+  private readonly hostEl = inject(ElementRef);
+
+  constructor() {
+    this.onClickListener = this.onClickListener.bind(this);
+    afterNextRender(() => this.initListener());
+  }
+
+  ngOnDestroy(): void {
+    this.removeListener();
+  }
+
+  private initListener() {
+    this.ngZone.runOutsideAngular(() => {
+      this.document.addEventListener('click', this.onClickListener);
+    });
+  }
+
+  private removeListener() {
+    this.document.removeEventListener('click', this.onClickListener);
+  }
+
+  private onClickListener(evt: Event) {
+    if (!this.clickOutsideEnabled) {
+      return;
+    }
+
+    if (!this.hostEl.nativeElement.contains(evt.target)) {
+      this.ngZone.run(() => this.clickOutside.emit(evt));
+    }
+  }
+}
