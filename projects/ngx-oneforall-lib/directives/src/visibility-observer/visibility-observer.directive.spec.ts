bea910abae5e6a252b59e2aca141cb2503325320
LPSingh <lsk18mto@gmail.com>
2025-06-06 22:22:37 -0400

    feat: added visibility observer directive

---
+++ projects/ngx-oneforall-lib/directives/src/visibility-observer/visibility-observer.directive.spec.ts
@@ -0,0 +1,8 @@
+import { VisibilityObserverDirective } from './visibility-observer.directive';
+
+describe('VisibilityObserverDirective', () => {
+  it('should create an instance', () => {
+    const directive = new VisibilityObserverDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ngx-oneforall-lib/directives/src/visibility-observer/visibility-observer.directive.ts
@@ -0,0 +1,76 @@
+import {
+  afterNextRender,
+  Directive,
+  ElementRef,
+  inject,
+  input,
+  OnDestroy,
+  output,
+} from '@angular/core';
+
+export type VisibilityChange =
+  | {
+      isVisible: true;
+      target: HTMLElement;
+    }
+  | {
+      isVisible: false;
+      target: HTMLElement | undefined;
+    };
+
+@Directive({
+  selector: '[visibilityObserver]',
+})
+export class VisibilityObserverDirective implements OnDestroy {
+  threshold = input<number>(1.0);
+  root = input<HTMLElement | null>(null);
+  visibilityChange = output<VisibilityChange>();
+
+  private readonly hostEl = inject(ElementRef);
+  private observer: IntersectionObserver | undefined;
+  private isVisible = false;
+
+  constructor() {
+    afterNextRender(() => this.connectObserver());
+  }
+
+  ngOnDestroy(): void {
+    this.disconnectObserver();
+    this.visibilityChange.emit({
+      isVisible: false,
+      target: undefined,
+    });
+  }
+
+  private connectObserver() {
+    this.disconnectObserver();
+
+    this.observer = new IntersectionObserver(entries => {
+      entries.forEach(
+        entry => {
+          const { isIntersecting: isVisible, target } = entry;
+          const hasChangedVisibility = isVisible !== this.isVisible;
+          if (hasChangedVisibility) {
+            this.visibilityChange.emit({
+              isVisible,
+              target: target as HTMLElement,
+            });
+          }
+        },
+        {
+          root: this.root(),
+          threshold: this.threshold(),
+        }
+      );
+    });
+
+    this.observer.observe(this.hostEl.nativeElement);
+  }
+
+  private disconnectObserver() {
+    if (this.observer) {
+      this.observer.disconnect();
+      this.observer = undefined;
+    }
+  }
+}
