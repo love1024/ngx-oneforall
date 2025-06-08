8ff4d3815d39796b7b6d49f62ec179cf3a30cdca
LPSingh <lsk18mto@gmail.com>
2025-06-07 23:02:50 -0400

    feat: added demo for visiblity change directive

--- projects/ngx-oneforall-docs/src/app/categories/directives/pages/visibility-change/demo/visibility-change-demo.component.ts
+++ projects/ngx-oneforall-docs/src/app/categories/directives/pages/visibility-change/demo/visibility-change-demo.component.ts
@@ -8,22 +8,36 @@ import {
   selector: 'lib-visibility-change-demo',
   imports: [VisibilityChangeDirective],
   template: `
-    @if (show()) {
-      <div (visibilityChange)="visibilityChange($event)"></div>
+    <div class="parent">
+      <div>
+        @if (show()) {
+          <div (visibilityChange)="visibilityChange($event)" class="box">
+            Scroll Down To Hide
+          </div>
+        }
+        <button (click)="showHide()">{{ show() ? 'Remove' : 'Add' }}</button>
+      </div>
+    </div>
+    @if (evt(); as e) {
+      <div class="logs">
+        <div><span>Visibility:</span> {{ e.isVisible }}</div>
+        <div>
+          <span>Removed From DOM:</span> {{ e.target ? 'false' : 'true' }}
+        </div>
+      </div>
     }
-
-    <button (click)="hide()">Hide</button>
   `,
   styleUrl: './visibility-change-demo.component.scss',
 })
 export class VisibilityChangeDemoComponent {
   show = signal(true);
+  evt = signal<VisibilityChange | null>(null);
 
-  hide() {
-    this.show.set(false);
+  showHide() {
+    this.show.update(v => !v);
   }
 
   visibilityChange(e: VisibilityChange) {
-    console.log(e);
+    this.evt.set(e);
   }
 }
