c01a9bc1b7e1bbe48ed9b380917f77b3b0d23096
LPSingh <lsk18mto@gmail.com>
2025-06-09 21:43:09 -0400

    feat: added auto focus directive

---
+++ projects/ngx-oneforall-lib/directives/src/focus/auto-focus.directive.ts
@@ -0,0 +1,48 @@
+import {
+  afterNextRender,
+  Directive,
+  effect,
+  ElementRef,
+  EnvironmentInjector,
+  inject,
+  model,
+  runInInjectionContext,
+} from '@angular/core';
+
+@Directive({
+  selector: '[autoFocus]',
+  host: {
+    '(focus)': 'hostFocused()',
+    '(blur)': 'hostBlured()',
+  },
+})
+export class AutoFocusDirective {
+  isFocused = model<boolean>(true);
+
+  private hostEl = inject<ElementRef<HTMLElement>>(ElementRef);
+  private environmentInjector = inject(EnvironmentInjector);
+
+  constructor() {
+    afterNextRender(() => {
+      this.focus();
+
+      runInInjectionContext(this.environmentInjector, () => {
+        effect(() => this.focus());
+      });
+    });
+  }
+
+  hostFocused() {
+    this.isFocused.set(true);
+  }
+
+  hostBlured() {
+    this.isFocused.set(false);
+  }
+
+  private focus() {
+    if (this.isFocused()) {
+      this.hostEl?.nativeElement.focus();
+    }
+  }
+}
