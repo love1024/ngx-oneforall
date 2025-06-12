c5c36ce5a137c32680db49f06560a048af49bdd7
LPSingh <lsk18mto@gmail.com>
2025-06-11 21:41:18 -0400

    feat: exposed auto focus directive

--- projects/ngx-oneforall-lib/directives/src/public_api.ts
+++ projects/ngx-oneforall-lib/directives/src/public_api.ts
@@ -1,3 +1,4 @@
 export * from './resized/resized.directive';
 export * from './click-outside/click-outside.directive';
 export * from './visibility-change/visibility-change.directive';
+export * from './auto-focus/auto-focus.directive';
