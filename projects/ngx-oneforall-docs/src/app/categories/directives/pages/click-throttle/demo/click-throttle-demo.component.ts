36048d9a102280826e172b7c898315240ad5e0b0
LPSingh <lsk18mto@gmail.com>
2025-06-13 22:08:54 -0400

    chore: added doc structure for click throttle component

---
+++ projects/ngx-oneforall-docs/src/app/categories/directives/pages/click-throttle/demo/click-throttle-demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-click-throttle-demo',
+  imports: [],
+  template: ` <p>click-throttle-demo works!</p> `,
+  styleUrl: './click-throttle-demo.component.scss',
+})
+export class ClickThrottleDemoComponent {}
---
+++ projects/ngx-oneforall-docs/src/app/categories/directives/pages/click-throttle/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import DirectivesCategory from '../../ng-doc.category';
+
+const ClickThrottleDirective: NgDocPage = {
+  title: 'Click Throttle',
+  mdFile: './index.md',
+  category: DirectivesCategory,
+  demos: {},
+};
+
+export default ClickThrottleDirective;
