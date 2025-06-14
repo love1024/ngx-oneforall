76bac76d0c3a71a8c0aec5be855691d509f87ec5
LPSingh <lsk18mto@gmail.com>
2025-06-13 22:09:20 -0400

    feat: exposed click throttle directive

--- projects/ngx-oneforall-lib/directives/src/public_api.ts
+++ projects/ngx-oneforall-lib/directives/src/public_api.ts
@@ -2,3 +2,4 @@ export * from './resized/resized.directive';
 export * from './click-outside/click-outside.directive';
 export * from './visibility-change/visibility-change.directive';
 export * from './auto-focus/auto-focus.directive';
+export * from './click-throttle/click-throttle.directive';
