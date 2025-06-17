ac7947e4d01ddb68932953a087fca0a915da0fb4
LPSingh <lsk18mto@gmail.com>
2025-06-16 22:19:13 -0400

    feat: added test cases for hover class directive

--- projects/ngx-oneforall-lib/directives/src/hover-class/hover-class.directive.spec.ts
+++ projects/ngx-oneforall-lib/directives/src/hover-class/hover-class.directive.spec.ts
@@ -1,8 +1,70 @@
+import { Component, Renderer2, signal } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HoverClassDirective } from './hover-class.directive';
 
+@Component({
+  imports: [HoverClassDirective],
+  template: `<div [hoverClass]="classes()"></div>`,
+})
+class TestHostComponent {
+  classes = signal('hovered active');
+}
+
 describe('HoverClassDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverClassDirective();
+  let fixture: ComponentFixture<TestHostComponent>;
+  let hostEl: HTMLElement;
+  let directive: HoverClassDirective;
+  let renderer: Renderer2;
+  let component: TestHostComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TestHostComponent, HoverClassDirective],
+      providers: [Renderer2],
+    });
+    fixture = TestBed.createComponent(TestHostComponent);
+    fixture.detectChanges();
+    hostEl = fixture.nativeElement.querySelector('div');
+    component = fixture.componentInstance;
+    directive =
+      fixture.debugElement.children[0].injector.get(HoverClassDirective);
+    renderer = fixture.debugElement.children[0].injector.get(Renderer2);
+  });
+
+  it('should create the directive', () => {
     expect(directive).toBeTruthy();
+  });
+
+  it('should add classes on mouseenter', () => {
+    const addClassSpy = jest.spyOn(renderer, 'addClass');
+    directive.onMouseEnter();
+    expect(addClassSpy).toHaveBeenCalledWith(hostEl, 'hovered');
+    expect(addClassSpy).toHaveBeenCalledWith(hostEl, 'active');
+  });
+
+  it('should remove classes on mouseleave', () => {
+    const removeClassSpy = jest.spyOn(renderer, 'removeClass');
+    directive.onMouseLeave();
+    expect(removeClassSpy).toHaveBeenCalledWith(hostEl, 'hovered');
+    expect(removeClassSpy).toHaveBeenCalledWith(hostEl, 'active');
+  });
+
+  it('should add new classes when hoverClass input changes', () => {
+    component.classes.set('foo bar');
+    fixture.detectChanges();
+    directive.onMouseEnter();
+
+    expect(hostEl.classList).toContain('foo');
+    expect(hostEl.classList).toContain('bar');
+  });
+
+  it('should ignore empty or whitespace-only classes', () => {
+    component.classes.set('foo   bar   ');
+
+    fixture.detectChanges();
+    directive.onMouseEnter();
+
+    expect(hostEl.classList).toContain('foo');
+    expect(hostEl.classList).toContain('bar');
   });
 });
