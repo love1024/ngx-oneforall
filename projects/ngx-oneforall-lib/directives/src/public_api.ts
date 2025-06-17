e2ce34464f04186324a4995154226c4d7e5b4038
LPSingh <lsk18mto@gmail.com>
2025-06-16 22:01:27 -0400

    feat: exposed hover class directive from lib

--- projects/ngx-oneforall-lib/directives/src/public_api.ts
+++ projects/ngx-oneforall-lib/directives/src/public_api.ts
@@ -3,3 +3,4 @@ export * from './click-outside/click-outside.directive';
 export * from './visibility-change/visibility-change.directive';
 export * from './auto-focus/auto-focus.directive';
 export * from './click-throttle/click-throttle.directive';
+export * from './hover-class/hover-class.directive';
