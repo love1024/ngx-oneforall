90517df363e6e44dea9136a16b6d7c43cc7b0441
LPSingh <lsk18mto@gmail.com>
2025-06-23 16:17:40 -0400

    feat: added demo for repeat directive

---
+++ projects/ngx-oneforall-docs/src/app/categories/directives/pages/repeat/demo/repeat-demo.component.ts
@@ -0,0 +1,27 @@
+import { Component, signal } from '@angular/core';
+import { RepeatDirective } from '@ngx-oneforall/directives';
+
+@Component({
+  selector: 'lib-repeat-demo',
+  standalone: true,
+  imports: [RepeatDirective],
+  template: `
+    <div class="repeat-demo-container">
+      <h2>Repeat Directive Demo</h2>
+      <p
+        *repeat="count(); let i = index; let isFirst = first; let isLast = last"
+        class="box">
+        <span class="index-label">Item #{{ i + 1 }}</span>
+        @if (isFirst) {
+          <span class="badge first">First item</span>
+        } @else if (isLast) {
+          <span class="badge last">Last item</span>
+        }
+      </p>
+    </div>
+  `,
+  styleUrl: 'repeat-demo.component.scss',
+})
+export class RepeatDemoComponent {
+  count = signal(5);
+}
