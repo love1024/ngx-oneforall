2cfafaab5ba6211490fb5c501b0c401963e57916
LPSingh <lsk18mto@gmail.com>
2025-07-07 18:22:30 -0400

    chore: added doc for route param signal

---
+++ projects/ngx-oneforall-docs/src/app/categories/signals/pages/route-param-signal/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import SignalsCategory from '../../ng-doc.category';
+
+const StorageSignal: NgDocPage = {
+  title: 'Route Param Signal',
+  mdFile: './index.md',
+  category: SignalsCategory,
+  demos: {},
+};
+
+export default StorageSignal;
