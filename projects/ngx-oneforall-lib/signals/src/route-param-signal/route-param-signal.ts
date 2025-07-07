074892f8243e3c4d3d65892d1ec75983585d6d80
LPSingh <lsk18mto@gmail.com>
2025-07-07 18:54:22 -0400

    feat: updated route param signal

--- projects/ngx-oneforall-lib/signals/src/route-param-signal/route-param-signal.ts
+++ projects/ngx-oneforall-lib/signals/src/route-param-signal/route-param-signal.ts
@@ -1,19 +1,12 @@
-import { effect, inject, signal, Signal } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 export function routeParamSignal(paramName: string): Signal<string | null> {
-  const router = inject(Router);
   const route = inject(ActivatedRoute);
 
-  const param = signal<string | null>(route.snapshot.paramMap.get(paramName));
-  effect(() => {
-    const sub = router.events
-      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
-      .subscribe(() => param.set(route.snapshot.paramMap.get(paramName)));
-
-    return () => sub.unsubscribe();
+  return toSignal(route.paramMap.pipe(map(pm => pm.get(paramName))), {
+    initialValue: route.snapshot.paramMap.get(paramName),
   });
-
-  return param.asReadonly();
 }
