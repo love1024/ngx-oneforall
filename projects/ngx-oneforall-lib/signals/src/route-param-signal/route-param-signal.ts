98a7da7e48152733fe18cece0360a4bedf63f96f
LPSingh <lsk18mto@gmail.com>
2025-07-09 20:37:54 -0400

    feat: added route params map signal

--- projects/ngx-oneforall-lib/signals/src/route-param-signal/route-param-signal.ts
+++ projects/ngx-oneforall-lib/signals/src/route-param-signal/route-param-signal.ts
@@ -1,6 +1,6 @@
 import { inject, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map } from 'rxjs';
 
 export function routeParamSignal(paramName: string): Signal<string | null> {
@@ -8,5 +8,13 @@ export function routeParamSignal(paramName: string): Signal<string | null> {
 
   return toSignal(route.paramMap.pipe(map(pm => pm.get(paramName))), {
     initialValue: route.snapshot.paramMap.get(paramName),
+  });
+}
+
+export function routeParamsMapSignal(): Signal<ParamMap> {
+  const route = inject(ActivatedRoute);
+
+  return toSignal(route.paramMap, {
+    initialValue: route.snapshot.paramMap,
   });
 }
