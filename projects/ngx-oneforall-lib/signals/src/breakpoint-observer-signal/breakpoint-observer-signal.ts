cec1e7c73734bc5f72db2019ed5a374245c5a2f8
LPSingh <lsk18mto@gmail.com>
2025-07-15 19:58:08 -0400

    feat: updated breakpoint observer signal

--- projects/ngx-oneforall-lib/signals/src/breakpoint-observer-signal/breakpoint-observer-signal.ts
+++ projects/ngx-oneforall-lib/signals/src/breakpoint-observer-signal/breakpoint-observer-signal.ts
@@ -1,21 +1,54 @@
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { inject, Signal } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { computed, inject, Signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Breakpoint, BreakpointQueries } from '@ngx-oneforall/constants';
-import { isKeyDefined } from '@ngx-oneforall/utils';
+
+export interface BreakpointResult {
+  matches: boolean;
+  breakpoints: Record<string, boolean>;
+}
 
 export function breakpointObserverSignal(
-  breakpoints: Breakpoint | Breakpoint[]
-): Signal<BreakpointState> {
+  breakpoints: Breakpoint | string | (Breakpoint | string)[]
+): Signal<BreakpointResult> {
   const breakpointObserver = inject(BreakpointObserver);
+  const reverseMap = new Map<string, Breakpoint>();
+
   const queries = (
     Array.isArray(breakpoints) ? breakpoints : [breakpoints]
-  ).map(b => (isKeyDefined(BreakpointQueries, b) ? BreakpointQueries[b] : b));
+  ).map(b => {
+    if (isBreakpoint(b)) {
+      reverseMap.set(BreakpointQueries[b], b);
+      return BreakpointQueries[b];
+    }
+    return b;
+  });
 
-  return toSignal(breakpointObserver.observe(queries), {
+  const observer = toSignal(breakpointObserver.observe(queries), {
     initialValue: {
       matches: false,
       breakpoints: {},
     },
   });
+
+  return computed(() => {
+    const response = observer();
+    const reversedBreakpoints: Record<string, boolean> = {};
+    Object.entries(response.breakpoints).forEach(([query, value]) => {
+      const breakpoint = reverseMap.get(query);
+      if (breakpoint) {
+        reversedBreakpoints[breakpoint] = value;
+      } else {
+        reversedBreakpoints[query] = value;
+      }
+    });
+    return {
+      matches: response.matches,
+      breakpoints: reversedBreakpoints,
+    } satisfies BreakpointResult;
+  });
+}
+
+function isBreakpoint(value: any): value is Breakpoint {
+  return value in Breakpoint;
 }
--- projects/ngx-oneforall-lib/signals/src/public_api.ts
+++ projects/ngx-oneforall-lib/signals/src/public_api.ts
@@ -1,3 +1,4 @@
 export * from './storage-signal/storage-signal';
 export * from './route-param-signal/route-param-signal';
 export * from './route-query-param-signal/route-query-param-signal';
+export * from './breakpoint-observer-signal/breakpoint-observer-signal';
