44ff6068a1cab59983f9dee7a210d1352dd4fb12
LPSingh <lsk18mto@gmail.com>
2025-07-17 22:48:07 -0400

    fix: removed unused code in breakpoint matcher

--- projects/ngx-oneforall-lib/signals/src/breakpoint-matcher-signal/breakpoint-matcher-signal.ts
+++ projects/ngx-oneforall-lib/signals/src/breakpoint-matcher-signal/breakpoint-matcher-signal.ts
@@ -46,55 +46,6 @@ export function breakpointMatcherSignal(
   return state.asReadonly();
 }
 
-// export function breakpointMatcherMultipleSignal(
-//   breakpoints: BreakpointInput[]
-// ): Signal<BreakpointResult> {
-//   const platform = inject(Platform);
-//   const state = signal<BreakpointResult>({
-//     matches: false,
-//     breakpoints: {},
-//   });
-//   if (platform.isBrowser && window.matchMedia) {
-//   }
-
-//   const breakpointObserver = inject(BreakpointObserver);
-//   const reverseMap = new Map<string, Breakpoint>();
-
-//   const queries = (
-//     Array.isArray(breakpoints) ? breakpoints : [breakpoints]
-//   ).map(b => {
-//     if (isBreakpoint(b)) {
-//       reverseMap.set(BreakpointQueries[b], b);
-//       return BreakpointQueries[b];
-//     }
-//     return b;
-//   });
-
-//   const observer = toSignal(breakpointObserver.observe(queries), {
-//     initialValue: {
-//       matches: false,
-//       breakpoints: {},
-//     },
-//   });
-
-//   return computed(() => {
-//     const response = observer();
-//     const reversedBreakpoints: Record<string, boolean> = {};
-//     Object.entries(response.breakpoints).forEach(([query, value]) => {
-//       const breakpoint = reverseMap.get(query);
-//       if (breakpoint) {
-//         reversedBreakpoints[breakpoint] = value;
-//       } else {
-//         reversedBreakpoints[query] = value;
-//       }
-//     });
-//     return {
-//       matches: response.matches,
-//       breakpoints: reversedBreakpoints,
-//     } satisfies BreakpointResult;
-//   });
-// }
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isBreakpoint(value: any): value is Breakpoint {
   return value in Breakpoint;
