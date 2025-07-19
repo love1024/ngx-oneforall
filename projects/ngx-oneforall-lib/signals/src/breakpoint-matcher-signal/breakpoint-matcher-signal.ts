80f1e5ec98e7ddc3cb8f7bfa0f27a0ecd05fc123
LPSingh <lsk18mto@gmail.com>
2025-07-18 23:43:18 -0400

    feat: added breakpoint matchner signal multiple

--- projects/ngx-oneforall-lib/signals/src/breakpoint-matcher-signal/breakpoint-matcher-signal.ts
+++ projects/ngx-oneforall-lib/signals/src/breakpoint-matcher-signal/breakpoint-matcher-signal.ts
@@ -46,6 +46,59 @@ export function breakpointMatcherSignal(
   return state.asReadonly();
 }
 
+export function breakpointMatcherMultipleSignal(
+  breakpoints: BreakpointInput[]
+): Signal<BreakpointResult> {
+  const platformId = inject(PLATFORM_ID);
+  const state = signal<BreakpointResult>({ matches: false, breakpoints: {} });
+  if (isPlatformBrowser(platformId) && window.matchMedia) {
+    const queries = breakpoints.map(b =>
+      isBreakpoint(b) ? BreakpointQueries[b] : b
+    );
+
+    queries.forEach(query => {
+      const mediaQuery = window.matchMedia(query);
+      state.set({
+        matches: state().matches || mediaQuery.matches,
+        breakpoints: {
+          ...state().breakpoints,
+          query: mediaQuery.matches,
+        },
+      });
+
+      const handler = (event: MediaQueryListEvent) => {
+        state.set({
+          matches: state().matches || event.matches,
+          breakpoints: {
+            ...state().breakpoints,
+            query: event.matches,
+          },
+        });
+      };
+
+      // Added just for unsubscription
+      effect(() => {
+        if ('addEventListener' in mediaQuery) {
+          mediaQuery.addEventListener('change', handler);
+        } else {
+          // @ts-expect-error deprecated API
+          mediaQuery.addListener(handler);
+        }
+
+        return () => {
+          if ('removeEventListener' in mediaQuery) {
+            mediaQuery.removeEventListener('change', handler);
+          } else {
+            // @ts-expect-error deprecated API
+            mediaQuery.removeListener(handler);
+          }
+        };
+      });
+    });
+  }
+  return state;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isBreakpoint(value: any): value is Breakpoint {
   return value in Breakpoint;
