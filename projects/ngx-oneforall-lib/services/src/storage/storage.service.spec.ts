824c3148625b22c882d96d2668bc47be04e8952f
LPSingh <lsk18mto@gmail.com>
2025-08-04 14:57:26 -0400

    test: added unit test for storage service

--- projects/ngx-oneforall-lib/services/src/storage/storage.service.spec.ts
+++ projects/ngx-oneforall-lib/services/src/storage/storage.service.spec.ts
@@ -1,16 +1,63 @@
-import { TestBed } from '@angular/core/testing';
-
 import { WebStorageService } from './web-storage.service';
 
-describe('StorageService', () => {
-  let service: WebStorageService;
+describe('WebStorageService', () => {
+  let storage: Storage;
+  let service: WebStorageService<unknown>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WebStorageService);
+    const store: Record<string, string> = {};
+    storage = {
+      getItem: jest.fn((key: string) => (key in store ? store[key] : null)),
+      setItem: jest.fn((key: string, value: string) => {
+        store[key] = value;
+      }),
+      removeItem: jest.fn((key: string) => {
+        delete store[key];
+      }),
+      clear: jest.fn(() => {
+        Object.keys(store).forEach(key => delete store[key]);
+      }),
+      key: jest.fn(),
+      length: 0,
+    };
+    service = new WebStorageService(storage);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should set and get a value', () => {
+    service.set('foo', { bar: 123 });
+    expect(storage.setItem).toHaveBeenCalledWith(
+      'foo',
+      JSON.stringify({ bar: 123 })
+    );
+    expect(service.get('foo')).toEqual({ bar: 123 });
+  });
+
+  it('should return undefined for missing key', () => {
+    expect(service.get('missing')).toBeUndefined();
+  });
+
+  it('should return true for has() if key exists', () => {
+    service.set('foo', 1);
+    expect(service.has('foo')).toBe(true);
+  });
+
+  it('should return false for has() if key does not exist', () => {
+    expect(service.has('nope')).toBe(false);
+  });
+
+  it('should remove a key', () => {
+    service.set('foo', 1);
+    service.remove('foo');
+    expect(storage.removeItem).toHaveBeenCalledWith('foo');
+    expect(service.get('foo')).toBeUndefined();
+  });
+
+  it('should clear all keys', () => {
+    service.set('foo', 1);
+    service.set('bar', 2);
+    service.clear();
+    expect(storage.clear).toHaveBeenCalled();
+    expect(service.get('foo')).toBeUndefined();
+    expect(service.get('bar')).toBeUndefined();
   });
 });
