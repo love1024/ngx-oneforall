a73b8b1ab03454a2d58580e27352bae502a8668a
LPSingh <lsk18mto@gmail.com>
2025-08-05 23:53:14 -0400

    feat: added set method in cookie service

--- projects/ngx-oneforall-lib/services/src/cookie/cookie.service.ts
+++ projects/ngx-oneforall-lib/services/src/cookie/cookie.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 
+export type SameSiteOption = 'Strict' | 'Lax' | 'None';
+
+export interface CookieOptions {
+  sameSite?: SameSiteOption;
+  domain?: string;
+  path?: string;
+  secure?: boolean;
+}
+
 @Injectable()
 export class CookieService {
   get(name: string): string {
@@ -23,6 +32,34 @@ export class CookieService {
     );
   }
 
+  set(name: string, value: string, options?: CookieOptions) {
+    const mergedOptions = this.mergeDefaultCookieOptions(options);
+    let cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)};`;
+
+    cookieString += `sameSite=${mergedOptions.sameSite};`;
+
+    if (mergedOptions?.path) {
+      cookieString += `path=${mergedOptions.path};`;
+    }
+    if (mergedOptions?.domain) {
+      cookieString += `domain=${mergedOptions.domain};`;
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Cookies#security
+    if (mergedOptions?.secure === false && mergedOptions.sameSite === 'None') {
+      mergedOptions.secure = true;
+      console.warn(
+        '[ngx-oneforall] - Cookie Service: Setting secure flag. As per Mozilla security document, if SameSite=None is set then the Secure attribute must also be set.'
+      );
+    }
+
+    if (mergedOptions?.secure) {
+      cookieString += `secure;`;
+    }
+
+    document.cookie = cookieString;
+  }
+
   private getCookieUsingRegex(name: string): RegExp {
     const escapedName = name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     return new RegExp(`(?:^|;\\s*)${escapedName}=(.*?)(?:;|$)`);
@@ -34,5 +71,12 @@ export class CookieService {
     } catch {
       return encodedString;
     }
+  }
+
+  private mergeDefaultCookieOptions(options?: CookieOptions): CookieOptions {
+    return {
+      sameSite: options?.sameSite ?? 'Lax',
+      ...options,
+    } satisfies CookieOptions;
   }
 }
