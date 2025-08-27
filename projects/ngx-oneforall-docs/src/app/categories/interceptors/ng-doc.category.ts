dcd08540cda97a166eeac313fb173e74758a66aa
LPSingh <lsk18mto@gmail.com>
2025-08-27 19:33:57 -0400

    chore: added doc for jwt interceptor

---
+++ projects/ngx-oneforall-docs/src/app/categories/interceptors/ng-doc.category.ts
@@ -0,0 +1,7 @@
+import { NgDocCategory } from '@ng-doc/core';
+
+const InterceptorsCategory: NgDocCategory = {
+  title: 'Interceptors',
+};
+
+export default InterceptorsCategory;
---
+++ projects/ngx-oneforall-docs/src/app/categories/interceptors/pages/jwt/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import InterceptorsCategory from '../../ng-doc.category';
+
+const JwtInterceptor: NgDocPage = {
+  title: 'Jwt Interceptor',
+  mdFile: './index.md',
+  category: InterceptorsCategory,
+  demos: {},
+};
+
+export default JwtInterceptor;
