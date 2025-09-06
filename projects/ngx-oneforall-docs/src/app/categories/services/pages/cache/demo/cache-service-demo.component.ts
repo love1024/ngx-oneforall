93cb64051278e14d767749ee3f43a83fe84dda78
LPSingh <lsk18mto@gmail.com>
2025-09-05 22:43:50 -0400

    feat: added demo for cache service

--- projects/ngx-oneforall-docs/src/app/categories/services/pages/cache/demo/cache-service-demo.component.ts
+++ projects/ngx-oneforall-docs/src/app/categories/services/pages/cache/demo/cache-service-demo.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CacheService, provideCacheService } from '@ngx-oneforall/services';
 
 @Component({
   selector: 'lib-cache-service-demo',
-  imports: [],
-  template: ` <p>cache-service-demo works!</p> `,
-  styleUrl: './cache-service-demo.component.scss',
+  template: `
+    <div class="cache-demo-container">
+      <h2>Promo Code</h2>
+      <div class="promo-code">{{ code() }}</div>
+      <span class="promo-info">Available for <strong>10 seconds</strong></span>
+      <div class="availability">
+        <div class="label">Availability:</div>
+        <span
+          [class.available]="isAvailable() === true"
+          [class.unavailable]="isAvailable() === false">
+          {{
+            isAvailable() === null
+              ? '-'
+              : isAvailable()
+                ? 'Available'
+                : 'Unavailable'
+          }}
+        </span>
+      </div>
+      <button class="check-btn" (click)="checkAgain()">
+        Check Availability
+      </button>
+    </div>
+  `,
+  styleUrl: 'cache-service-demo.component.scss',
+  providers: [
+    provideCacheService({
+      ttl: 10000,
+    }),
+  ],
 })
-export class CacheServiceDemoComponent {}
+export class CacheServiceDemoComponent {
+  code = signal('b25lZm9yYWxs');
+  isAvailable = signal<null | boolean>(null);
+
+  private cacheService = inject(CacheService);
+
+  constructor() {
+    this.cacheService.set('isAvailable', true);
+  }
+
+  checkAgain(): void {
+    this.isAvailable.set(this.cacheService.get('isAvailable') ?? false);
+    setTimeout(() => this.isAvailable.set(null), 2000);
+  }
+}
