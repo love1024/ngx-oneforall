97991f91fc4659990374000a46ddb3739d7d8bc1
Lovepreet Singh <lsk18mto@gmail.com>
2025-11-29 18:19:39 -0500

    feat: added host platforms constant

---
+++ projects/ngx-oneforall-lib/constants/src/host-platforms.ts
@@ -0,0 +1,9 @@
+export enum HostPlatform {
+    MAC = 'MAC',
+    IOS = 'IOS',
+    ANDROID = 'ANDROID',
+    WINDOWS = 'WINDOWS',
+    LINUX = 'LINUX',
+    WINDOWS_PHONE = 'WINDOWS_PHONE',
+    UNKNOWN = 'UNKNOWN',
+}
