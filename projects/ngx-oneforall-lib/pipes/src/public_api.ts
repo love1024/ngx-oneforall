503324c9ee8a210930c8aabc3c78092ebe110d7b
Lovepreet Singh <lsk18mto@gmail.com>
2025-12-28 02:21:55 -0500

    Remove commented secondary entry point listings and add a default empty export.

--- projects/ngx-oneforall-lib/pipes/src/public_api.ts
+++ projects/ngx-oneforall-lib/pipes/src/public_api.ts
@@ -1,11 +1,1 @@
-// Secondary entry points:
-// - @ngx-oneforall/pipes/bytes
-// - @ngx-oneforall/pipes/call
-// - @ngx-oneforall/pipes/first-error-key
-// - @ngx-oneforall/pipes/highlight-search
-// - @ngx-oneforall/pipes/memoize
-// - @ngx-oneforall/pipes/pluralize
-// - @ngx-oneforall/pipes/range
-// - @ngx-oneforall/pipes/safe-html
-// - @ngx-oneforall/pipes/time-ago
-// - @ngx-oneforall/pipes/truncate
+export default {};
