15353834ec68d10f109f124de7dfe692c77dbda5
LPSingh <lsk18mto@gmail.com>
2026-01-07 19:47:32 -0500

    feat: added structure for mask directive

--- projects/ngx-oneforall-lib/directives/mask/src/mask.directive.spec.ts
+++ projects/ngx-oneforall-lib/directives/mask/src/mask.directive.spec.ts
@@ -1,8 +1,30 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { MaskDirective } from './mask.directive';
 
+@Component({
+  template: `<input [mask]="'###-###-####'" />`,
+  imports: [MaskDirective],
+})
+class TestHostComponent {}
+
 describe('MaskDirective', () => {
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestHostComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new MaskDirective();
+    const directive = fixture.debugElement
+      .query(By.directive(MaskDirective))
+      .injector.get(MaskDirective);
     expect(directive).toBeTruthy();
   });
 });
