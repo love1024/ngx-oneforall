000786e0c09770386524bbe5e79cacb1771c41c2
Lovepreet Singh <lsk18mto@gmail.com>
2026-01-09 01:52:15 -0500

    feat: added mask input

--- projects/ngx-oneforall-lib/directives/mask/src/mask.directive.ts
+++ projects/ngx-oneforall-lib/directives/mask/src/mask.directive.ts
@@ -2,7 +2,19 @@ import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: '[mask]',
+  host: {
+    '(input)': 'onInput($event)',
+  },
 })
 export class MaskDirective {
   mask = input.required<string>();
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    input.value = this.maskValue(input.value);
+  }
+
+  private maskValue(value: string) {
+    return value.replace(/[^0-9]/g, '');
+  }
 }
