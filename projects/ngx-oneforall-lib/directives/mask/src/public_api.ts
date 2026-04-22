01bb1e2f91188e70fa66295ca798f3b4e84350ac
Lovepreet Singh <lsk18mto@gmail.com>
2026-04-21 21:47:07 -0400

    feat: implement core masking engine and add support for mask pipes (#25)
    * feat: implement core masking engine and add support for mask pipes
    
    * docs: add mask pipe to README documentation tables

--- projects/ngx-oneforall-lib/directives/mask/src/public_api.ts
+++ projects/ngx-oneforall-lib/directives/mask/src/public_api.ts
@@ -1,3 +1,5 @@
 export * from './mask.directive';
 export { MaskQuantifier, patterns } from './mask.config';
 export type { IConfigPattern } from './mask.config';
+export { applyMask, validateMask } from './mask.engine';
+export type { MaskConfig, MaskResult } from './mask.engine';
